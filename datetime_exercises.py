cfd3beaba551bb4c4a98f425ee6ef4848f1ac318
meco454 <96034840+meco454@users.noreply.github.com>
2023-01-16 12:42:34 +0800

    some datetime exercises

---
+++ datetime_exercises.py
@@ -0,0 +1,62 @@
+import datetime
+help(datetime.date)
+date1 = datetime.date(2021,1,1)
+date2 = datetime.date(day = 31,month = 7,year = 2021)
+date3 = datetime.date(1990, month = 5, day = 7)
+print(date1)
+print(date2)
+print(date3)
+
+
+help(datetime.time)
+t1 = datetime.time(12,0,0)
+t2 = datetime.time(6,30,0)
+t3 = datetime.time(9,15,0)
+
+print(t1)
+print(t2)
+print(t3)
+
+
+d1 = datetime.datetime(2020, 7, 20, 11, 30)
+d2 = datetime.datetime(1990, 3, 10, 12)
+d3 = datetime.datetime(2021, 1, 1)
+print(d1)
+print(d2)
+print(d3)
+
+
+str_d1 = "2020-07-21" 
+str_d2 = "2020-12-31"
+d1 = datetime.datetime.strptime(str_d1, "%Y-%m-%d")
+d2 = datetime.datetime.strptime(str_d2, "%Y-%m-%d")
+
+
+d3 = d2 - d1
+
+print(f"Number of days: {d3.days}")
+
+
+
+d1 = datetime.datetime.strptime("Jul 20 2020 11:30:00", "%b %d %Y %H:%M:%S")
+d2 = datetime.datetime.strptime("2021-02-20 10:25:00", "%Y-%m-%d %H:%M:%S")
+print(d2-d1)
+
+
+
+d1 = datetime.datetime.strptime("2021-04-20 11:30:00", "%Y-%m-%d %H:%M:%S")
+
+print(d1.strftime("%Y-%m-%d"))
+print(d1.strftime("%d-%m-%Y"))
+print(d1.strftime("%m-%Y"))
+print(d1.strftime("%B-%Y"))
+print(d1.strftime("%d %B, %Y"))
+print(d1.strftime("%Y-%m-%d %H:%M:%S"))
+print(d1.strftime("%m/%d/%y %H:%M:%S"))
+print(d1.strftime("%d(%a) %B %Y"))
+
+
+
+
+
+
